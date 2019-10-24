4892cdbbf47b96b9949c814558901d9baa6f7815
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-10-25 00:19:46 +0100

    Add heading one component

---
+++ packages/component-library/src/text/text.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HeadingOne from './headingOne.component.tsx';
+
+export default { title: 'Typography' };
+
+export const headingOne = () => (
+  <HeadingOne text="Some Larger Text"/>
+);
