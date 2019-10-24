3c4f535762812b39bc95e901b6a6ed7676913dc6
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-10-25 00:58:23 +0100

    Fix storybook config

--- packages/component-library/.storybook/config.ts
+++ packages/component-library/.storybook/config.ts
@@ -1,9 +1,3 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../src/', true, /.stories.tsx$/);
-
-function loadStories() {
-  req.keys().forEach((filename) => req(filename));
-}
-
-configure(loadStories, module);
+configure(require.context('../src/', true, /\.stories\.tsx$/), module)
